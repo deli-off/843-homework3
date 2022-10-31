a8c4a643d14669582865b9eadd736a8e492d2ebf
deli-off <dzamsedovnazif1@gmail.com>
2022-10-31 21:14:01 +0500

    star

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// let UserName = prompt('Как вас зовут?');
+
+// if (UserName === 'Alex') {
+//     let account = prompt('Номер счёта')
+
+//     if (account === '7777') {
+//         let money = prompt('Сколько обналичить?')
+
+//         if (money <= 10000) {
+//             alert(10000 - money)
+//         } else {
+//             alert('Недостаточно средств')
+//         }
+//     } else {
+//         alert('Пользователь ненайден')
+//     }
+// } else {
+//     alert('Пользователь ненайден')
+// }
